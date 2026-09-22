c38f89eca892a4e7c65b1b8ac817e03301c26850
Peter Svensson <psvensson@gmail.com>
2026-09-22 12:53:27 +0200

    raft port: own canonical lifecycle event vocabulary

--- src/raft/raft-operation-port-constants.js
+++ src/raft/raft-operation-port-constants.js
@@ -1,3 +1,14 @@
+const RAFT_EVENT = Object.freeze({
+  DATA: 'data',
+  LEADER: 'leader',
+  FOLLOWER: 'follower',
+  CANDIDATE: 'candidate',
+  LEADER_CHANGE: 'leader change',
+  COMMIT: 'commit',
+  TERM_CHANGE: 'term change',
+  COMMITTED_PREFIX_DIVERGENCE: 'committed prefix divergence',
+});
+
 const RAFT_OPERATION_OUTCOME = Object.freeze({
   CORE_OK: 'CORE_OK',
   CORE_REFUSED: 'CORE_REFUSED',
@@ -17,6 +28,7 @@ const RAFT_MEMBERSHIP_RESERVATION_OUTCOME = Object.freeze({
 });
 
 export {
+  RAFT_EVENT,
   RAFT_MEMBERSHIP_OPERATION,
   RAFT_MEMBERSHIP_RESERVATION_OUTCOME,
   RAFT_OPERATION_OUTCOME,
